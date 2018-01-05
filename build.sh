540fdd9f76079183e16e303c6c659a6823e1547b
KeepPass <35096046+KeepPass@users.noreply.github.com>
2018-01-04 17:34:05 -0700

    Added build files for windows and shell
    Added batch and shell scripts for windows and linux initialization support.

---
+++ build.sh
@@ -0,0 +1,19 @@
+git clone https://github.com/KeepPass/keep_pass_store.git Store
+mv Store/KeepPass.jar KeepPass.jar
+echo "Got KeepPass.jar"
+git clone https://github.com/KeepPass/keep_pass_get.git Get
+mv Get/GetPass.jar GetPass.jar
+echo "Got GetPass.jar"
+git clone https://github.com/KeepPass/keep_pass_binaries.git Bin
+mv Bin/keep.cpp keep.cpp
+mv Bin/get.cpp get.cpp
+echo "Got the binaries"
+g++ keep.cpp -o keep
+g++ get.cpp -o get
+echo "Compiled c++ files"
+cd ..
+rm -R Bin Get Store
+#Lets clean up here
+echo "Cleaning up..."
+rm -R keep.cpp
+rm -R get.cpp
